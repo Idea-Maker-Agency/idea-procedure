2d0fb331f5087314f0d2e3caf2475db70628ff4a
Thomas Kwon <tdkwon@gmail.com>
2021-12-07 21:07:34 -0800

    updated

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -11,7 +11,7 @@ from fabric import task, Connection
 
 @task
 def doit(ctx, ip, keypath):
-    ctx.user = 'django'
+    ctx.user = 'azureuser'
     ctx.host = ip
     ctx.connect_kwargs.key_filename = ''.format(keypath)
 
@@ -26,9 +26,9 @@ def django(ctx):
     print('********* apt-get install python3-django python3-pip virtualenv postgresql nginx -y ******')
     conn.sudo('apt-get install python3-django python3-pip virtualenv postgresql nginx -y', pty=True)
     print('************** add-apt-repository ppa:certbot/certbot ******************')
-    conn.sudo('add-apt-repository ppa:certbot/certbot -y', pty=True)
+    # deprecated conn.sudo('add-apt-repository ppa:certbot/certbot -y', pty=True)
     print('*************** apt-get install python-certbot-nginx -y *****************')
-    conn.sudo('apt-get install python-certbot-nginx -y', pty=True)
+    conn.sudo('apt-get install python3-certbot-nginx -y', pty=True)
     print('*************************** useradd -m django ***************************')
     conn.sudo('useradd -m django')
     print('********************** usermod -s /bin/bash django **********************')
